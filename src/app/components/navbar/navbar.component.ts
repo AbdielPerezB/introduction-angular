fbd27088ef0add277576f1381fdfad31357d188e
Abdiel <abdiel2012p@gmail.com>
2024-02-02 13:49:08 -0600

    estructuras de secuencia v1 listo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,17 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent implements OnInit{
+export class NavbarComponent {
   public rutas =[
     {name: 'Home', path: '/home'},
     {name: 'Perfil', path: '/perfil'},
     {name: 'Estructuras de Secuencia', path: '/estructuras-secuencia'},
     {name: 'Docs', path: '/docs'}
   ];
-  public existHome:boolean = true;
-  ngOnInit(): void {
-    this.existHome = this.rutas[0].name == 'Home'? true : false;
-    console.log(this.existHome);
-  }
 
 }
